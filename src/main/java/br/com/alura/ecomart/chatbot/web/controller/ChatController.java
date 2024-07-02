6c1d2f7c0a0809d7f0c919047238b4795d66ec54
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-07-02 15:08:38 -0300

    feat: create chatbot service with stream

--- src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
+++ src/main/java/br/com/alura/ecomart/chatbot/web/controller/ChatController.java
@@ -1,24 +1,40 @@
 package br.com.alura.ecomart.chatbot.web.controller;
 
+import br.com.alura.ecomart.chatbot.ChatbotApplication;
+import br.com.alura.ecomart.chatbot.domain.service.ChatbotService;
 import br.com.alura.ecomart.chatbot.web.dto.PerguntaDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
 
 @Controller
 @RequestMapping({"/", "chat"})
 public class ChatController {
 
     private static final String PAGINA_CHAT = "chat";
+    private ChatbotService service;
+
+    public ChatController(ChatbotService service) {
+        this.service = service;
+    }
 
     @GetMapping
     public String carregarPaginaChatbot() {
         return PAGINA_CHAT;
     }
 
-    @PostMapping
+    @PostMapping()
     @ResponseBody
-    public String responderPergunta(@RequestBody PerguntaDto dto) {
-        return dto.pergunta();
+    public ResponseBodyEmitter responderPergunta(@RequestBody PerguntaDto dto) {
+        var fluxoResposta = service.responderPergunta(dto.pergunta());
+        var emitter = new ResponseBodyEmitter();
+        fluxoResposta.subscribe(chunk -> {
+            var token = chunk.getChoices().get(0).getMessage().getContent();
+            if (token != null) {
+                emitter.send(token);
+            }
+        }, emitter::completeWithError, emitter::complete);
+        return emitter;
     }
 
     @GetMapping("limpar")
